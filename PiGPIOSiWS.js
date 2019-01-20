cf09e1c5cc4f0950af0eec10d752d32b2bf52320
cymplecy <simplecy@googlemail.com>
2019-01-20 16:08:43 +0000

    Update PiGPIOSiWS.js

--- PiGPIOSiWS.js
+++ PiGPIOSiWS.js
@@ -24,9 +24,9 @@ class PiGPIOSiWS {
           opcode: 'sghBroadcast',
           blockType: Scratch.BlockType.COMMAND,
           
-          text: 'PiGPIOSi broadcast [URL]',
+          text: 'PiGPIOSi broadcast [BC]',
           arguments: {
-            URL: {
+            BC: {
               type: Scratch.ArgumentType.STRING,
               defaultValue: ''
             }
@@ -80,54 +80,7 @@ class PiGPIOSiWS {
     }
   }
 
-  isExactly({A, B}) {
-    return A === B;
-  }
-
-  isLessOrEqual({A, B}) {
-    return A <= B;
-  }
-
-  isMoreOrEqual({A, B}) {
-    return A >= B;
-  }
-
-  trueBlock() {
-    return true;
-  }
-
-  falseBlock() {
-    return false;
-  }
-
-  exponent({A, B}) {
-    return Math.pow(A, B);
-  }
-
-  pi() {
-    return Math.PI;
-  }
-
-  ternaryOperator({A, B, C}) {
-    return A ? B : C;
-  }
-
-  letters({STRING, START, END}) {
-    return STRING.slice(Math.max(1, START) - 1, Math.min(STRING.length, END));
-  }
-
-  currentMillisecond() {
-    return Date.now() % 1000;
-  }
-
-  fetchFrom({URL}) {
-    return new Promise(resolve => {
-      fetch('https://translate-service.scratch.mit.edu/translate?lang=fr&text=' + URL).then(res => res.text()).then(resolve)
-      .catch(err => resolve(''));
-    });
-  }
-  
-  sghBroadcast({URL}) {
+  sghBroadcast({BC}) {
     new Promise(resolve => {
       fetch('https://translate-service.scratch.mit.edu/translate?lang=fr&text=' + URL).then(res => res.text()).then(resolve)
       .catch(err => resolve(''));
@@ -147,35 +100,6 @@ class PiGPIOSiWS {
       .catch(err => resolve(''));
     });
   }    
-
-  parseJSON({PATH, JSON_STRING}) {
-    try {
-      const path = PATH.toString().split('/').map(prop => decodeURIComponent(prop));
-      if (path[0] === '') path.splice(0, 1);
-      if (path[path.length - 1] === '') path.splice(-1, 1);
-      let json;
-      try {
-        json = JSON.parse(' ' + JSON_STRING);
-      } catch (e) {
-        return e.message;
-      }
-      path.forEach(prop => json = json[prop]);
-      if (json === null) return 'null';
-      else if (json === undefined) return '';
-      else if (typeof json === 'object') return JSON.stringify(json);
-      else return json.toString();
-    } catch (err) {
-      return '';
-    }
-  }
-
-  stringToBoolean({STRING}) {
-    return STRING;
-  }
-
-  regexReplace({STRING, REGEX, NEWSTRING}) {
-    return STRING.toString().replace(new RegExp(REGEX, 'gi'), NEWSTRING);
-  }
 
 }
 
