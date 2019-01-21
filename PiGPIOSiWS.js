5c26e243cbde261187c1388c823b2eb0c52715c8
cymplecy <simplecy@googlemail.com>
2019-01-21 07:22:16 +0000

    Update PiGPIOSiWS.js

--- PiGPIOSiWS.js
+++ PiGPIOSiWS.js
@@ -5,7 +5,7 @@ const icon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34
 class PiGPIOSiWS {
   constructor() {
     this.ws = new WebSocket('wss://translate-service.scratch.mit.edu:8000/')
-    ws.onopen = function(evt) { onOpen(evt) }
+    this.ws.onopen = function(evt) { onOpen(evt) }
   }
   function onOpen(evt) {
     console.log('websocket opened')
