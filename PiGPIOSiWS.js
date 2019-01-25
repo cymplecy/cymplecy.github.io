a37cd753241a49d92e6f69109a9f7069d66eb2b1
cymplecy <simplecy@googlemail.com>
2019-01-25 11:24:26 +0000

    Update PiGPIOSiWS.js

--- PiGPIOSiWS.js
+++ PiGPIOSiWS.js
@@ -17,7 +17,7 @@ class PiGPIOSiWS {
       sensorDict[sensorSplit[0]] = sensorSplit[1];
       console.log("sensorDict:" + sensorDict);
     };
-    setInterval(function(){ console.log("refresh sent"); }, 1000);
+    setInterval(function(){ this.ws.send('refresh'); console.log("refresh sent"); }, 1000);
     //this.ws.send('refresh');
 
   }
